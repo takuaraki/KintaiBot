ef75e22a52bb6709d0e91fba9e78fa1d29690ce6
takuaraki <takuaraki7@gmail.com>
2018-09-05 03:02:45 +0900

    send to slack kintai info

--- src/Slack.gs
+++ src/Slack.gs
@@ -1,7 +1,7 @@
-function sendToSlack(message) {
+function sendToSlack(channelId, message) {
     var token = PropertiesService.getScriptProperties().getProperty('SLACK_ACCESS_TOKEN');
     var slackApp = SlackApp.create(token);
-    slackApp.postMessage("#bot-test", message, {
+    slackApp.postMessage(channelId, message, {
         username: "KintaiBot",
         icon_url: "https://www.silhouette-illust.com/wp-content/uploads/2016/11/17380-300x300.jpg"
     });
--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,25 @@ import { TargetDateExtractor } from './TargetDateExtractor';
 
 declare var global: any;
 
-global.doPost = (event: SlackPostEvent): void => {
-  saveKintai(event.text);
+global.doPost = (event: PostEvent): void => {
+  var text = event.parameter['text'];
+  var userName = event.parameter['user_name'];
+  saveKintai(userName, text);
 };
 
-function saveKintai(text: string) {
+function saveKintai(userName: string, text: string) {
   var now = new Date();
   var kintaiService = new KintaiService(now);
   var targetDateExtractor = new TargetDateExtractor(now);
   var targetDate = targetDateExtractor.extract(text);
   kintaiService.register(new KintaiInfo(targetDate, 'test', text));
-  sendToSlack('save kintai');
+  sendToSlack(
+    '#bot-test',
+    `I saved Kintai. \`name: ${userName} , date: ${targetDate}, text: ${text}\``
+  );
 }
 
-class SlackPostEvent {
+class PostEvent {
   queryString: string;
   parameter: { [key: string]: string };
   parameters: { [key: string]: [string] };
@@ -28,10 +33,6 @@ class SlackPostEvent {
     contents: string;
     name: string;
   };
-
-  get text(): string {
-    return this.parameter['text'];
-  }
 }
 
-declare function sendToSlack(text: string);
+declare function sendToSlack(channelId: string, text: string);
