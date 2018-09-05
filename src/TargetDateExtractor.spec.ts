76c2570fcd837a05b4a66b3f884f68f1bb46917b
takuaraki <takuaraki7@gmail.com>
2018-09-05 23:55:15 +0900

    lint

--- src/TargetDateExtractor.spec.ts
+++ src/TargetDateExtractor.spec.ts
@@ -1,71 +1,60 @@
-import { TargetDateExtractor } from './TargetDateExtractor'
+import { TargetDateExtractor } from './TargetDateExtractor';
 jest.unmock('./TargetDateExtractor');
 
 describe('TargetDateExtractor', () => {
-    let now = new Date()
-    let tester = new TargetDateExtractor(now)
-    describe('extract', () => {
-        it('slash divide case1', () => {
-            expect(tester.extract('【A休申請】9/12 私用のため'))
-            .toBe(`${now.getFullYear()}/9/12`)
-        })
-        it('slash divide case2', () => {
-            expect(tester.extract('【A休申請】2018/9/12 私用のため'))
-            .toBe(`${now.getFullYear()}/9/12`)
-        })
-        it('slash divide case3', () => {
-            expect(tester.extract('【A休申請】09/12 私用のため'))
-            .toBe(`${now.getFullYear()}/09/12`)
-        })
-        it('slash divide case4', () => {
-            expect(tester.extract('【A休申請】09/02 私用のため'))
-            .toBe(`${now.getFullYear()}/09/02`)
-        })
-        it('slash divide case5', () => {
-            expect(tester.extract('【A休申請】2018/09/02 私用のため'))
-            .toBe(`${now.getFullYear()}/09/02`)
-        })
+  let now = new Date();
+  let tester = new TargetDateExtractor(now);
+  describe('extract', () => {
+    it('slash divide case1', () => {
+      expect(tester.extract('【A休申請】9/12 私用のため')).toBe(`${now.getFullYear()}/9/12`);
+    });
+    it('slash divide case2', () => {
+      expect(tester.extract('【A休申請】2018/9/12 私用のため')).toBe(`${now.getFullYear()}/9/12`);
+    });
+    it('slash divide case3', () => {
+      expect(tester.extract('【A休申請】09/12 私用のため')).toBe(`${now.getFullYear()}/09/12`);
+    });
+    it('slash divide case4', () => {
+      expect(tester.extract('【A休申請】09/02 私用のため')).toBe(`${now.getFullYear()}/09/02`);
+    });
+    it('slash divide case5', () => {
+      expect(tester.extract('【A休申請】2018/09/02 私用のため')).toBe(`${now.getFullYear()}/09/02`);
+    });
 
-        it('gappi divide case1', () => {
-            expect(tester.extract('【A休申請】9月12日 私用のため'))
-            .toBe(`${now.getFullYear()}/9/12`)
-        })
-        it('gappi divide case2', () => {
-            expect(tester.extract('【A休申請】2018年9月12日 私用のため'))
-            .toBe(`${now.getFullYear()}/9/12`)
-        })
-        it('gappi divide case3', () => {
-            expect(tester.extract('【A休申請】09月12日 私用のため'))
-            .toBe(`${now.getFullYear()}/09/12`)
-        })
-        it('gappi divide case4', () => {
-            expect(tester.extract('【A休申請】09月02日 私用のため'))
-            .toBe(`${now.getFullYear()}/09/02`)
-        })
-        it('gappi divide case5', () => {
-            expect(tester.extract('【A休申請】2018年09月02日 私用のため'))
-            .toBe(`${now.getFullYear()}/09/02`)
-        })
+    it('gappi divide case1', () => {
+      expect(tester.extract('【A休申請】9月12日 私用のため')).toBe(`${now.getFullYear()}/9/12`);
+    });
+    it('gappi divide case2', () => {
+      expect(tester.extract('【A休申請】2018年9月12日 私用のため')).toBe(
+        `${now.getFullYear()}/9/12`
+      );
+    });
+    it('gappi divide case3', () => {
+      expect(tester.extract('【A休申請】09月12日 私用のため')).toBe(`${now.getFullYear()}/09/12`);
+    });
+    it('gappi divide case4', () => {
+      expect(tester.extract('【A休申請】09月02日 私用のため')).toBe(`${now.getFullYear()}/09/02`);
+    });
+    it('gappi divide case5', () => {
+      expect(tester.extract('【A休申請】2018年09月02日 私用のため')).toBe(
+        `${now.getFullYear()}/09/02`
+      );
+    });
 
-        it('hyphen divide case1', () => {
-            expect(tester.extract('【A休申請】9-12 私用のため'))
-            .toBe(`${now.getFullYear()}/9/12`)
-        })
-        it('hyphen divide case2', () => {
-            expect(tester.extract('【A休申請】2018-9-12 私用のため'))
-            .toBe(`${now.getFullYear()}/9/12`)
-        })
-        it('hyphen divide case3', () => {
-            expect(tester.extract('【A休申請】09-12 私用のため'))
-            .toBe(`${now.getFullYear()}/09/12`)
-        })
-        it('hyphen divide case4', () => {
-            expect(tester.extract('【A休申請】09-02 私用のため'))
-            .toBe(`${now.getFullYear()}/09/02`)
-        })
-        it('hyphen divide case5', () => {
-            expect(tester.extract('【A休申請】2018-09-02 私用のため'))
-            .toBe(`${now.getFullYear()}/09/02`)
-        })
-    })
-})
+    it('hyphen divide case1', () => {
+      expect(tester.extract('【A休申請】9-12 私用のため')).toBe(`${now.getFullYear()}/9/12`);
+    });
+    it('hyphen divide case2', () => {
+      expect(tester.extract('【A休申請】2018-9-12 私用のため')).toBe(`${now.getFullYear()}/9/12`);
+    });
+    it('hyphen divide case3', () => {
+      expect(tester.extract('【A休申請】09-12 私用のため')).toBe(`${now.getFullYear()}/09/12`);
+    });
+    it('hyphen divide case4', () => {
+      expect(tester.extract('【A休申請】09-02 私用のため')).toBe(`${now.getFullYear()}/09/02`);
+    });
+    it('hyphen divide case5', () => {
+      expect(tester.extract('【A休申請】2018-09-02 私用のため')).toBe(`${now.getFullYear()}/09/02`);
+    });
+  });
+});
