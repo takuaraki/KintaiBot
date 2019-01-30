0329383b49fe063f99832216e01d32dbafb34475
takuaraki <takuaraki7@gmail.com>
2019-01-31 03:19:43 +0900

    interactive data

--- src/main/index_interactive.ts
+++ src/main/index_interactive.ts
@@ -5,9 +5,5 @@ declare let global: any;
 global.doPost = (event: PostEvent): object => {
   return ContentService.createTextOutput()
     .setMimeType(ContentService.MimeType.JSON)
-    .setContent(
-      JSON.stringify({
-        text: 'delete kintai.'
-      })
-    );
+    .setContent(event.postData.contents);
 };
